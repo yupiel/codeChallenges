4089375cf58dd0225f5067ceb4a0ff3551f055d3
Stefan Wilke <stefan.wilke@asideas.de>
2019-12-11 18:00:55 +0100

    third challenge, first year

---
+++ src/main/java/azubi/challenge/firstyear/SumPair.java
@@ -0,0 +1,33 @@
+package main.java.azubi.challenge.firstyear;
+
+public class SumPair {
+
+    /**
+     * Für eine beliebige Anzahl an positiven Zahlen,
+     * finde ein Päarchen, deren Summe der gegegeben Summe entspricht
+     * und gib eine Erfolgsmeldung (true) zurück.
+     * 
+     * Falls kein Päarchen existiert, gebe false zurück.
+     * 
+     * Beispiel 1:
+     *   numbers = [10, 3, 15, 15, 15, 7]
+     *   sum = 17
+     *   result = true // 10 + 7 == 17
+     * 
+     * Beispiel 2:
+     *   numbers = [1]
+     *   sum = 17
+     *   result = false // es gibt keine Summe mit nur einer Zahl
+     * 
+     * Beispiel 3:
+     *   numbers = [1, 2]
+     *   sum = 17
+     *   result = false // es gibt kein Päarchen mit der Summe 17
+     * 
+     * @return true, if one pairs' total equals given sum
+     */
+    public static boolean sumPairExists(int sum, int[] numbers) {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+}
---
+++ src/test/java/azubi/challenge/firstyear/SumPairTest.java
@@ -0,0 +1,28 @@
+package test.java.azubi.challenge.firstyear;
+
+import org.junit.Test;
+import static org.junit.Assert.*; 
+
+import main.java.azubi.challenge.firstyear.SumPair;
+
+public class SumPairTest {
+
+    @Test
+    public void findSumPair_shouldReturn_false_ifNoNumbersGiven() {
+        boolean actual = SumPair.sumPairExists(17, new int[0]);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void findSumPair_shouldReturn_false_ifNoPairIsFound() {
+        boolean actual = SumPair.sumPairExists(17, new int[] {1, 2, 3});
+        assertFalse(actual);
+    }
+
+    @Test
+    public void findSumPair_shouldReturn_true_ifAPair_isFound() {
+        boolean actual = SumPair.sumPairExists(17, new int[] {10, 15, 3, 7});
+        assertTrue(actual);
+    }
+
+}
