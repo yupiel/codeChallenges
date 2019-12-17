2afc184de058dde2a801219b20fe33eab5dd445f
Stefan Wilke <stefan.wilke@asideas.de>
2019-12-17 17:06:05 +0100

    next challenge

---
+++ src/main/java/azubi/challenge/firstyear/PermutedMultiples.java
@@ -0,0 +1,25 @@
+package main.java.azubi.challenge.firstyear;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class PermutedMultiples {
+
+    public static void main(String...args) {
+        findSmallestPermutation();
+    }
+
+    /**
+     * Given the positive number 125874 .. if you multiply that by 2 (which is 251748) and
+     * compare both numbers you will notice that both share the same digits, meaning they are permutations.
+     * 
+     * x = 125874
+     * x2 = 251748
+     * Permutation!
+     * 
+     * Find the smallest positive integer x, such that x2, x3, x4, x5 and x6 are all permutations!!
+     */
+    public static void findSmallestPermutation() {
+        // TODO IMPLEMENT ME!
+    }
+}
